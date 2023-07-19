98aacc29543dffc3bf10f2abc06690ed8745f2bf
Jinglong Zhu <820909823@qq.com>
2023-07-19 14:18:40 +1200

    添加注释

--- main.py
+++ main.py
@@ -1,50 +1,40 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.metrics import mean_squared_error, mean_absolute_error, max_error, mean_squared_log_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error, max_error
 from math import sqrt
 
-# Load the observed data
+# 定义文件路径
 met_path = '/Users/zhu/Desktop/w2-postprocessing/met.csv'
-data = pd.read_csv(met_path, delimiter=',', skiprows=1)
-
-# Load the model output data
 tsr_path = '/Users/zhu/Desktop/w2-postprocessing/tsr_1_seg31.csv'
+
+# 读取气象数据和模型数据
+data = pd.read_csv(met_path, delimiter=',', skiprows=1)
 model_data = pd.read_csv(tsr_path)
 model_data['JDAY'] = model_data['JDAY'].astype(int)
 
-# Merge the two datasets on "JDAY"
+# 合并数据
 merged_data = pd.merge(data[['JDAY', 'TAIR']], model_data[['JDAY', 'T2(C)']], on='JDAY', how='inner')
 
-# Rename the columns for clarity
+# 重命名列名
 merged_data.rename(columns={'TAIR': 'Observed_Temp', 'T2(C)': 'Model_Temp'}, inplace=True)
 
-# Calculate the Root Mean Squared Error
+# 计算均方根误差（RMSE）、平均绝对误差（MAE）和最大误差（Max Error）
 rmse = sqrt(mean_squared_error(merged_data['Observed_Temp'], merged_data['Model_Temp']))
-# Calculate the Mean Absolute Error
 mae = mean_absolute_error(merged_data['Observed_Temp'], merged_data['Model_Temp'])
-
-# Calculate the Max Error
 max_err = max_error(merged_data['Observed_Temp'], merged_data['Model_Temp'])
 
-# Create a new figure and set the size of the figure
+# 绘制图表
 plt.figure(figsize=(10, 6))
 
-# Plot observed temperature
 plt.plot(merged_data['JDAY'], merged_data['Observed_Temp'], label='Observed Temperature')
-
-# Plot model predicted temperature
 plt.plot(merged_data['JDAY'], merged_data['Model_Temp'], label='Model Predicted Temperature')
 
-# Set the labels and title
 plt.xlabel('Day of the Year')
 plt.ylabel('Temperature (C)')
 plt.title('Observed vs Model Predicted Temperature')
 
-# Add error metrics to the plot
+# 在图表中添加文本信息，显示RMSE、MAE和最大误差
 plt.text(0.02, 0.85, f'RMSE = {rmse:.2f}\nMAE = {mae:.2f}\nMax Error = {max_err:.2f}\n', transform=plt.gca().transAxes)
 
-# Add a legend
 plt.legend()
-
-# Show the plot
 plt.show()
